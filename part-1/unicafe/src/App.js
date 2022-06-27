62c0ac9d5418015a6c23bc37837a5330ef7b3702
Danil Nesterenko <nochzato@gmail.com>
2022-06-27 14:26:46 +0300

    part-1 completed

---
+++ part-1/unicafe/src/App.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+const Button = (props) => {
+  return <button onClick={props.onClick}>{props.text}</button>;
+};
+
+const StatisticLine = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+);
+
+const Statistics = ({ good, neutral, bad }) => {
+  if (good === 0 && neutral === 0 && bad === 0) {
+    return <p>No feedback</p>;
+  }
+  return (
+    <table>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine
+        text="average"
+        value={(good - bad) / (good + bad + neutral)}
+      />
+      <StatisticLine
+        text="positive"
+        value={(good / (good + bad + neutral)) * 100}
+      />{" "}
+    </table>
+  );
+};
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button text="good" onClick={() => setGood(good + 1)} />
+      <Button text="neutral" onClick={() => setNeutral(neutral + 1)} />
+      <Button text="bad" onClick={() => setBad(bad + 1)} />
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
+};
+
+export default App;
